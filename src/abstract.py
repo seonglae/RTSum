285b68937cb7c59161b8fef0bd391835212f5ee4
Yongki Cho <okcho9807@yonsei.ac.kr>
2023-05-15 04:18:05 +0900

    Senticification model training & inference

--- src/abstract.py
+++ src/abstract.py
@@ -1,5 +1,17 @@
 from src.extract import Triple, triple2sentence
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 
 
-def abstract(triples: list[Triple]) -> str:
-  return ' '.join(list(map(lambda triple: triple2sentence(triple, 10), triples)))
+def abstract(triples: list[Triple], model_checkpoint: str, device: str = "cpu") -> str:
+  # Load Model & Summarization Pipeline
+  tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+  model = AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint).to(device)
+  summarizer = pipeline("summarization", model=model, tokenizer=tokenizer, device=device)
+
+  # Make relation classes to a string
+  relations = ' '.join(list(map(lambda triple: triple2sentence(triple, 10), triples)))
+  if len(relations.split()) == 0: return
+
+  # Make a summarization
+  summary = summarizer(relations)[0]["summary_text"]
+  return summary
