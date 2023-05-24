98a42d6f17c4db69e194c9bc7dd6060aca8f7e56
seonglae <sungle3737@gmail.com>
2023-05-24 18:05:26 +0900

    feat: print rouge scores with top triple confidence & salient score

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def file(path: str) -> str:
   return text(document)
 
 
-def test(article_file: str, summary_file: str, start: int, end: int) -> None:
+def test(article_file: str, summary_file: str, start: int, end: int) -> str:
   with open(article_file, 'r', encoding='UTF8') as articles:
     with open(summary_file, 'r', encoding='UTF8') as summaries:
       if start is None:
@@ -23,15 +23,28 @@ def test(article_file: str, summary_file: str, start: int, end: int) -> None:
       if end is None:
         end = len(articles)
       gold_summaries = summaries.readlines()
+      mean_rouge1 = 0
+      mean_rouge2 = 0
+      mean_rouge_l = 0
       for i, article in enumerate(articles):
         if int(i) < start:
           continue
         if start <= int(i) < end:
-          target_lines = filter(lambda line: int(line.split('\t')[0]) == i, gold_summaries)
-          gold_summary = ' '.join([line.split('\t')[2].strip() for line in target_lines])
-          summarize_test(article, gold_summary)
+          target_lines = filter(lambda line: int(
+              line.split('\t')[0]) == i, gold_summaries)
+          gold_summary = ' '.join([line.split('\t')[2].strip()
+                                  for line in target_lines])
+          score = summarize_test(article, gold_summary)
+          mean_rouge1 += score['rouge1'].fmeasure
+          mean_rouge2 += score['rouge2'].fmeasure
+          mean_rouge_l += score['rougeL'].fmeasure
         else:
           break
+      return f"""Results
+      Rouge-1: {mean_rouge1 / (end - start)}
+      Rouge-2: {mean_rouge2 / (end - start)}
+      Rouge-L: {mean_rouge_l / (end - start)}
+      """
 
 
 def exportarticle(path: str, output: str, start: int, end: int) -> None:
--- src/abstract.py
+++ src/abstract.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 
-from src.extract import Triple
+from src.extract import Triple, triple2sentence
 
 
 def abstract(triples: List[Triple], model_checkpoint: str = 'sjyyj/sjyyj', device: str = "cpu") -> str:
@@ -12,6 +12,11 @@ def abstract(triples: List[Triple], model_checkpoint: str = 'sjyyj/sjyyj', devic
                         tokenizer=tokenizer, device=device)
 
   # Make relation classes to a string
+  print(f"""
+Score:{triples[0]['score']}, Confidence: {triples[0]['confidence']} - {triple2sentence(triples[0])}
+Score:{triples[1]['score']}, Confidence: {triples[1]['confidence']} - {triple2sentence(triples[1])}
+Score:{triples[2]['score']}, Confidence: {triples[2]['confidence']} - {triple2sentence(triples[2])}
+""")
   relations = ''.join(list(map(tokenize_triple, triples[:3])))
   if len(relations.split()) == 0:
     return 'Cannot abstract triples'
