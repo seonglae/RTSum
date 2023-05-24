96198dfa422a1e101fd2f9c972f2fb56494d8932
seonglae <sungle3737@gmail.com>
2023-05-24 15:23:58 +0900

    style: web.py red emphasis to restrict korean

--- web.py
+++ web.py
@@ -9,9 +9,9 @@ TITLE = '세줄요약좀'
 st.set_page_config(page_title=TITLE)
 st.header(TITLE)
 st.markdown('''
-### 영문 뉴스 복붙시 요약해주는 AI 챗봇입니다 ([CNN](https://edition.cnn.com/), [FoxNews](https://www.foxnews.com/)).
+### <span style="color: #ffaaaa">영문</span> 뉴스 복붙시 요약해주는 AI 챗봇입니다 ([CNN](https://edition.cnn.com/), [FoxNews](https://www.foxnews.com/)).
 일정  양 이상의 정보가 포함된 문장들을 적어야 제대로 작동합니다. (**한글 x**, [Source Code](https://github.com/sjyyj/sjyyj), [Model](https://huggingface.co/sjyyj/sjyyj))
-''')
+''', unsafe_allow_html=True)
 
 styl = f"""
 <style>
