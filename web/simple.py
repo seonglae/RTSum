2e98e46c0b25386739e4c9dfc4ae60ad1beeeffe
seonglae <sungle3737@gmail.com>
2023-08-05 00:03:44 +0900

    feat: phrase highlighting green without mathciing bug

--- web/simple.py
+++ web/simple.py
@@ -1,5 +1,6 @@
 from difflib import SequenceMatcher
 import asyncio
+import re
 
 import streamlit as st
 
@@ -43,15 +44,16 @@ def get_similar_part(source, similar):
   return None
 
 
-async def main(article):
+async def main(article: str):
   # Check Cache
   if article in st.session_state:
     print("Cache hit!")
-    response, sentences, triples = st.session_state[article]
+    response, sentences, triples, phrases = st.session_state[article]
   else:
     print(f"Input: {article}")
-    response, sentences, triples = await summarize(article)
-  st.session_state[article] = (response, sentences, triples)
+    response, sentences, triples, phrases = await summarize(article)
+    print(f"Output: {response}")
+  st.session_state[article] = (response, sentences, triples, phrases)
 
   # Summary
   st.write("## Summary")
@@ -60,7 +62,9 @@ async def main(article):
 
   # Highlight
   st.write("### Highlight")
-  emphasis_sentence = st.checkbox("**Sentence**", False)
+  col1, col2 = st.columns(2)
+  emphasis_sentence = col1.checkbox("**Sentence**", False)
+  emphasis_phrase = col2.checkbox("**Phrases**", False)
   col1, col2, col3, col4, col5 = st.columns(5)
   emphasis_triple = col1.checkbox("**Triple**", True)
   emphasis_subject = col2.checkbox("(Subject)", emphasis_triple)
@@ -68,12 +72,13 @@ async def main(article):
   emphasis_object = col4.checkbox("(Object)", emphasis_triple)
   emphasis_adverbs = col5.checkbox("(Adverbs)", emphasis_triple)
 
+  # 1. Highlight Sentence
   html_article = article
+  temp_article = article
   if emphasis_sentence:
-    temp_article = article
     for i, sentence in enumerate(sentences):
       match = get_similar_part(temp_article, sentence["text"])
-      temp_article = temp_article.replace(match, '')
+      temp_article = temp_article.replace(match, '', 1)
 
       if match:
         hexscore = hex(
@@ -86,19 +91,14 @@ async def main(article):
 
         border = "border-radius: 5px"
         html_sentence = sentence["text"].replace(
-            match, f"<span style='{border}; {background}'>{match}</span>{small}S{i+1}</span>")
-        html_article = html_article.replace(sentence["text"], html_sentence)
+            match, f"<span style='{border}; {background}'>{match}</span>{small}S{i+1}</span>", 1)
+        html_article = html_article.replace(sentence["text"], html_sentence, 1)
 
+  # 2. Highlight Triple
   for i, triple in enumerate(triples):
     if len(triple['extraction']['arg2s']) > 0:
       knowledge = triple2sentence(triple)
       match = get_similar_part(triple["parent"]["text"], knowledge)
-
-      print(sentences.index(triple['parent'])+1,
-            i +
-            1, triple['extraction']['arg1']['text'], '|', triple['extraction']['rel']['text'],
-            '|', triple['extraction']['arg2s'][0]['text'],
-            '\n', match)
 
       if match:
         hexscore = hex(int(255 / triples[0]["score"] * triple["score"]))[2:]
@@ -118,26 +118,47 @@ async def main(article):
           submatch = get_similar_part(
               temp_match, triple["extraction"]["arg1"]["text"])
           html_match = html_match.replace(
-              submatch, f"<span style='{border}; {background}; {color}'>{submatch}</span>{small}S{i+1}</span>")
+              submatch, f"<span style='{border}; {background}; {color}'>{submatch}</span>{small}S{i+1}</span>", 1)
         if emphasis_pred:
           submatch = get_similar_part(
               temp_match, triple["extraction"]["rel"]["text"])
           html_match = html_match.replace(
-              submatch, f"<span style='{border}; {background}; {color}'>{submatch}</span>{small}P{i+1}</span>")
+              submatch, f"<span style='{border}; {background}; {color}'>{submatch}</span>{small}P{i+1}</span>", 1)
         if emphasis_object:
           submatch = get_similar_part(
               temp_match, triple["extraction"]["arg2s"][0]["text"])
           html_match = html_match.replace(
-              submatch, f"<span style='{border}; {background}; {color}'>{submatch}</span>{small}O{i+1}</span>")
+              submatch, f"<span style='{border}; {background}; {color}'>{submatch}</span>{small}O{i+1}</span>", 1)
         if emphasis_adverbs and len(triple["extraction"]["arg2s"]) > 1:
           for adverb in triple["extraction"]["arg2s"][1:]:
-            submatch = get_similar_part(
-                temp_match, adverb["text"])
-            html_match = html_match.replace(
-                submatch, f"<span style='{border}; {background}; {color}'>{submatch}</span>{small}A{i+1}</span>")
+            if len(adverb["text"].strip().split(' ')) > 1:
+              submatch = get_similar_part(
+                  temp_match, adverb["text"])
+              html_match = html_match.replace(
+                  submatch, f"<span style='{border}; {background}; {color}'>{submatch}</span>{small}A{i+1}</span>", 1)
 
         if emphasis_subject or emphasis_pred or emphasis_object or emphasis_adverbs:
-          html_article = html_article.replace(match, html_match)
+          html_article = html_article.replace(match, html_match, 1)
+
+  # 2. Highlight Phrase
+  if emphasis_phrase:
+    maximum = phrases[0][1]
+    minimal = phrases[len(phrases) - 1][1]
+    for i, phrase in enumerate(list(filter(lambda p: len(p[0]) > 3, phrases))[:10]):
+      match = get_similar_part(article, phrase[0].strip())
+
+      if match:
+        hexscore = hex(
+            int(100 / (maximum - minimal) * phrase[1]))[2:]
+        background = f"background: #33bb66{hexscore}; padding: 0 2px"
+        smallborder = "border-radius: 2px"
+        position = "position: relative; left: 2px; top: 2px; padding: 0 1px"
+        size = "font-size: 3px"
+        small = f"<span style='{size}; {background}; {smallborder}; {position}'>"
+
+        border = "border-radius: 5px"
+        html_article = html_article.replace(
+            match, f"<span style='{border}; {background}'>{match}</span>{small}P{i+1}</span>", 1)
 
   st.write(f"{html_article}", unsafe_allow_html=True)
 
